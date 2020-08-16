056d71eb9e1d144475dfa8943f14034e4bb0e8a8
Ge Yang <yangge1987@gmail.com>
2020-08-16 03:20:42 -0700

    add updated gif

--- pg_analysis/metaworld_benchmarks/single_env.md
+++ pg_analysis/metaworld_benchmarks/single_env.md
@@ -7,20 +7,24 @@ We pick two environments:
 tasks = ["box-close-v1", "bin-picking-v1"]
 ```
 ```python
+rewards = []
 for task_name in tasks:
     ml1 = metaworld.ML1(task_name)  # Construct the benchmark, sampling tasks
 
+    frames = []
+    
     env = ml1.train_classes[task_name]()  # Create an environment with task `pick_place`
     env = RenderEnv(env)
     for t_id, task in enumerate(ml1.train_tasks):
         env.set_task(task)  # Set task
         obs = env.reset()  # Reset environment
         a = env.action_space.sample()  # Sample an action
-        frames = []
         for i in range(5):
-            obs, reward, done, info = env.step(a)  # Step the environoment with the sampled
+            obs, reward, done, info = env.step(a)  # Step the environment with the sampled
             img = env.render("rgb", width=240, height=160)
             frames.append(img)
+            rewards.append(rewards)
+
     doc.video(frames, f"videos/{task_name}_{t_id}.gif", caption=task_name)
 ```
 <div style="flex-wrap:nowrap; display:flex; flex-direction:row; item-align:center;"><div><div style="text-align: center">box-close-v1</div><img style="margin:0.5em;" src="videos/box-close-v1_49.gif" /></div><div><div style="text-align: center">bin-picking-v1</div><img style="margin:0.5em;" src="videos/bin-picking-v1_49.gif" /></div></div>
--- pg_analysis/metaworld_benchmarks/single_env.py
+++ pg_analysis/metaworld_benchmarks/single_env.py
@@ -13,8 +13,11 @@ if __name__ == '__main__':
     from env_wrappers.metaworld import RenderEnv
 
     with doc, doc.row(wrap=False):
+        rewards = []
         for task_name in tasks:
             ml1 = metaworld.ML1(task_name)  # Construct the benchmark, sampling tasks
+
+            frames = []
 
             env = ml1.train_classes[task_name]()  # Create an environment with task `pick_place`
             env = RenderEnv(env)
@@ -22,10 +25,16 @@ if __name__ == '__main__':
                 env.set_task(task)  # Set task
                 obs = env.reset()  # Reset environment
                 a = env.action_space.sample()  # Sample an action
-                frames = []
                 for i in range(5):
-                    obs, reward, done, info = env.step(a)  # Step the environoment with the sampled
+                    obs, reward, done, info = env.step(a)  # Step the environment with the sampled
                     img = env.render("rgb", width=240, height=160)
                     frames.append(img)
+                    rewards.append(rewards)
+
             doc.video(frames, f"videos/{task_name}_{t_id}.gif", caption=task_name)
 
+    with doc @ "Now show the reward distribution", doc.row(wrap=False):
+        import matplotlib.pyplot as plt
+
+        plt.hist(rewards, bins=10)
+        doc.savefig(f"figures/single_env/reward_dist.png")
