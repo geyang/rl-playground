6ae35ba04b2446d8a19387091fa317be039f1ff0
Ge Yang <yangge1987@gmail.com>
2021-08-23 14:47:40 -0400

    remove the treeqn implementation

--- amy_treeqn/utils/bench/__init__.py
+++
@@ -1,2 +0,0 @@
-from treeqn.utils.bench.benchmarks import *
-from treeqn.utils.bench.monitor import *
--- amy_treeqn/utils/bl_common/__init__.py
+++
@@ -1,2 +0,0 @@
-from treeqn.utils.bl_common.math_util import *
-from treeqn.utils.bl_common.misc_util import *
